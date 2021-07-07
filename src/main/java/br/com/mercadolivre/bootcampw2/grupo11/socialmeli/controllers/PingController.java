2acf4348bf6aec92fed46dc2d3be1305f8640294
Luiz Mariz <luiz.mariz@mercadolivre.com>
2021-07-07 13:57:59 -0300

    fix: set default ping controller

--- src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/controllers/PingController.java
+++ src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/controllers/PingController.java
@@ -1,21 +1,15 @@
-//package br.com.mercadolivre.bootcampw2.grupo11.socialmeli.controllers;
-//
-//import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.services.TestService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class PingController {
-//
-//
-//    @Autowired
-//    private TestService service;
-//
-//    @GetMapping("/ping")
-//    public ResponseEntity<String> ping() {
-//        service.test();
-//        return ResponseEntity.ok("pong");
-//    }
-//}
+package br.com.mercadolivre.bootcampw2.grupo11.socialmeli.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
