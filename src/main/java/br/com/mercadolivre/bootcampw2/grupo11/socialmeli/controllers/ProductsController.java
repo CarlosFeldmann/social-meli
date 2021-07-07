3e1cde3ad8d24eee20614aadf0b84442669037b5
Giovannapls <giovanna.polissici@mercadolivre.com>
2021-07-07 17:13:10 -0300

    [feat] Create new Post

--- src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/controllers/ProductsController.java
+++ src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/controllers/ProductsController.java
@@ -1,11 +1,15 @@
 package br.com.mercadolivre.bootcampw2.grupo11.socialmeli.controllers;
 
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.forms.CreatePostForm;
 import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.services.ProductsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
@@ -13,5 +17,15 @@ public class ProductsController {
 
     @Autowired
     private ProductsService productsService;
+
+    @Operation(description="Cria um novo post")
+    @ApiResponses(value = { @ApiResponse(responseCode = "201", description = "Post criado com sucesso"),
+                            @ApiResponse(responseCode = "400", description = "Erro nos dados enviados"),
+                            @ApiResponse(responseCode = "500", description = "Erro durante o processamento no servidor")})
+    @PostMapping("/newpost")
+    public ResponseEntity<HttpStatus> post(@RequestBody CreatePostForm Postform) {
+        productsService.createNewPost(Postform);
+        return (ResponseEntity<HttpStatus>) ResponseEntity.status(HttpStatus.CREATED);
+    }
 
 }
--- src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/services/ProductsService.java
+++ src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/services/ProductsService.java
@@ -1,9 +1,17 @@
 package br.com.mercadolivre.bootcampw2.grupo11.socialmeli.services;
 
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.entities.Post;
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.entities.Product;
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.entities.Seller;
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.exceptions.ResourceNotFoundException;
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.forms.CreatePostForm;
 import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.repositories.PostRepository;
 import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.repositories.ProductRepository;
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.repositories.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductsService {
@@ -13,4 +21,39 @@ public class ProductsService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private SellerRepository sellerRepository;
+
+    public Post createNewPost(CreatePostForm postForm){
+        Post newPost = createPostfromForm(postForm);
+        return postRepository.save(newPost);
+    }
+
+    public Optional<Seller> getSellerById(int id){
+        return sellerRepository.findById(id);
+    }
+
+    private Post createPostfromForm(CreatePostForm postForm){
+        Post newPost = new Post();
+        Product newProduct =  new Product();
+        var seller = getSellerById(postForm.getUserId());
+
+        if(seller.isPresent()) {
+            newPost.setSeller(seller.get());
+        }
+        else{
+            throw new ResourceNotFoundException("Seller Id", postForm.getUserId());
+        }
+
+        newPost.setCategory(postForm.getCategory());
+        newPost.setDate(postForm.getDate());
+        newProduct.setBrand(postForm.getDetail().getBrand());
+        newProduct.setColor(postForm.getDetail().getColor());
+        newProduct.setNotes(postForm.getDetail().getNotes());
+        newProduct.setProductName(postForm.getDetail().getProductName());
+        newProduct.setType(postForm.getDetail().getType());
+        newPost.setDetail(newProduct);
+        return newPost;
+    }
 }
