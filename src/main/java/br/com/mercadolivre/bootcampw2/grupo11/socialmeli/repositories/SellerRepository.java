4e078bfcd9669c6d57964efc3f5da37c0e68ac38
Victor Planas <victor.planas@mercadolivre.com>
2021-07-07 15:11:21 -0300

    feat: added repositories

---
+++ src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/repositories/SellerRepository.java
@@ -0,0 +1,20 @@
+package br.com.mercadolivre.bootcampw2.grupo11.socialmeli.repositories;
+
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.entities.Post;
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.entities.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SellerRepository extends JpaRepository<Seller, Integer> {
+    @Override
+    List<Seller> findAllById(Iterable<Integer> iterable);
+
+    Integer getFollowerCountBySellerId(Integer idSeller);
+
+    void unfollowSeller(Integer idUser, Integer idSeller);
+
+    List<Post> getPromoProductsBySellerId(Integer idSeller);
+}
---
+++ src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.bootcampw2.grupo11.socialmeli.repositories;
+
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Override
+    List<User> findAllById(Iterable<Integer> iterable);
+}
