c4168a74c0748dcff1fbaee24294ddc8615bd277
Luiz Mariz <luiz.mariz@mercadolivre.com>
2021-07-08 12:18:44 -0300

    feat(method): method created to remove Seller for followed Set

--- src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/entities/Customer.java
+++ src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/entities/Customer.java
@@ -1,5 +1,6 @@
 package br.com.mercadolivre.bootcampw2.grupo11.socialmeli.entities;
 
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.exceptions.ResourceNotFoundException;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import java.util.Objects;
 import java.util.Set;
 /**
  *  This entity inherits from user and encapsulates the logic of users following sellers
@@ -16,7 +18,7 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 public class Customer extends User{
-    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<FollowDate> followed;
 
     /**
@@ -24,8 +26,19 @@ public class Customer extends User{
      * @param seller - seller who will be followed
      */
     public void addFollow(Seller seller){
-        FollowDateKey tmpFollowDateKey = new FollowDateKey(this.getUserId(),seller.getUserId());
         FollowDate tmpFollowDate = new FollowDate(this, seller);
         followed.add(tmpFollowDate);
+    }
+
+    /**
+     *
+     * @param seller - seller who will be unfollowed
+     */
+    public void removeFollow(Seller seller){
+        FollowDate followMatch = followed.stream()
+                .filter(a -> a.getSeller().equals(seller))
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("Seller", seller.getUserId()));
+        followed.remove(followMatch);
     }
 }
