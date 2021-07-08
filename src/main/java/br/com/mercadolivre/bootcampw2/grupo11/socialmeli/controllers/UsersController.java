e46829c6ebc5d0e8a1c10538f06f443cb95a0c41
Luiz Mariz <luiz.mariz@mercadolivre.com>
2021-07-08 15:05:21 -0300

    feat(merge): resolve conflict

--- src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/controllers/UsersController.java
+++ src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/controllers/UsersController.java
@@ -1,12 +1,10 @@
 package br.com.mercadolivre.bootcampw2.grupo11.socialmeli.controllers;
 
-import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.dtos.GenericMessageDTO;
-import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.dtos.SellerFollowerListDTO;
-import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.dtos.UserDTO;
-import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.dtos.UserFollowingListDTO;
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.dtos.*;
 import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.forms.UserForm;
 import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.services.UsersService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
