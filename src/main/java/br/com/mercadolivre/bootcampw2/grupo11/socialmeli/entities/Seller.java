b77feba0d7d0aadad3fe8503ad392509c9411d18
Luiz Mariz <luiz.mariz@mercadolivre.com>
2021-07-08 10:06:25 -0300

    feat(minorFix): define cascade in relationship and optimize imports

--- src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/entities/Seller.java
+++ src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/entities/Seller.java
@@ -4,8 +4,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -19,6 +21,6 @@ public class Seller extends User{
     @OneToMany(mappedBy = "seller")
     private Set<FollowDate> followers;
 
-    @OneToMany(mappedBy = "seller")
+    @OneToMany(mappedBy = "seller", cascade = CascadeType.ALL)
     private Set<Post> posts;
 }
