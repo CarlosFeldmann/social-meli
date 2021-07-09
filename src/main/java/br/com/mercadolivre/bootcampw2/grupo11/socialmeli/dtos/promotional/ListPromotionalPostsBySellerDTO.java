37446520f37b3e14ea3b8ad2c5dfff7ba486c400
Luiz Mariz <luiz.mariz@mercadolivre.com>
2021-07-09 17:57:12 -0300

    Fix(duplicate): remove dup implementation ListPromotionalPostsBySellerDTO

--- src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/dtos/promotional/ListPromotionalPostsBySellerDTO.java
+++ src/main/java/br/com/mercadolivre/bootcampw2/grupo11/socialmeli/dtos/promotional/ListPromotionalPostsBySellerDTO.java
@@ -1,14 +1,16 @@
 package br.com.mercadolivre.bootcampw2.grupo11.socialmeli.dtos.promotional;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import br.com.mercadolivre.bootcampw2.grupo11.socialmeli.dtos.UserDTO;
+import lombok.Getter;
 
 import java.util.List;
 
-@AllArgsConstructor
-@Data
-public class ListPromotionalPostsBySellerDTO {
-    private int userId;
-    private String userName;
+@Getter
+public class ListPromotionalPostsBySellerDTO extends UserDTO {
     private List<PromotionalPostDTO> posts;
+
+    public ListPromotionalPostsBySellerDTO(int userId, String userName, List<PromotionalPostDTO> posts) {
+        super(userId, userName);
+        this.posts = posts;
+    }
 }
